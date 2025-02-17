1bbaecd1908ad949d25525ec9fd98beef73c56f0
kishorreyansh <kishor.andekar2389@gmail.com>
2025-02-16 21:57:47 -0600

    changed repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/KISHOR-PROFESSIONAL-PORTFOLIO/',
+  base: '/my-portfolio/',
   plugins: [react()],
 })
